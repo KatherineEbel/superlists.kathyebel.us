b4ae777d423b33f1eac090e68135a567c204c1b4
kathyebel <kathyebel@me.com>
2018-08-08 10:05:47 -0400

    Update JavaScript error handling code. Use dom ready function.

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,4 +1,7 @@
-const initialize = () => {
+const initialize =
+
+window.Superlists = {};
+window.Superlists.initialize = () => {
     $('input[name="text"]').on('keypress', () => {
         console.log('in keypress handler');
         $('.alert-danger').hide()
