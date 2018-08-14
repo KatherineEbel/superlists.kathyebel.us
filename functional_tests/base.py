dd275abfdbec3457ce2570a289cd1ac1d8c693c3
kathy <kathyebel@me.com>
2018-08-14 13:50:04 -0400

    Add FT for sharing lists

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,8 +1,11 @@
+from django.conf import settings
 from selenium.webdriver.common.keys import Keys
-from .server_tools import reset_database
+from .server_tools import reset_database, create_session_on_server
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException, NoSuchElementException
+from .management.commands.create_session import create_pre_authenticated_session
+from .server_tools import create_session_on_server
 import time
 import os
 from datetime import datetime
@@ -71,6 +74,21 @@ class FunctionalTest(StaticLiveServerTestCase):
             windowid=self._windowid,
             timestamp=timestamp
         )
+
+    def create_pre_authenticated_session(self, email):
+        if self.staging_server:
+            session_key = create_session_on_server(self.staging_server, email)
+        else:
+            session_key = create_pre_authenticated_session(email)
+
+        # # to set a cookie we need to first visit the domain.
+        # # 404 pages load the quickest!
+        self.browser.get(self.live_server_url + '/404_no_such_url/')  #
+        self.browser.add_cookie(dict(
+            name=settings.SESSION_COOKIE_NAME,
+            value=session_key,
+            path='/',
+        ))
 
     def add_list_item(self, item_text):
         num_rows = len(self.browser.find_elements_by_css_selector('#id_list_table tr'))
